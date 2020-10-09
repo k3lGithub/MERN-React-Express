50041dc9991bdec402c3711dc9dc17dabf493104
name <email>
2020-10-09 23:04:35 +1100

    dummy search

--- medical-centre-app/src/components/common/Search.js
+++ medical-centre-app/src/components/common/Search.js
@@ -1,8 +1,29 @@
 import React, { Component, useEffect } from "react";
+import TextField from "@material-ui/core/TextField";
 
+export default class Search extends Component {
+  state = {
+    query: "",
+  };
 
-export default function Search() {
+  handleInputChange = () => {
+    this.setState({
+      query: this.search.value,
+    });
+  };
 
-
-  render() 
+  render() {
+    return (
+      <form>
+        <TextField
+          id="search"
+          label="Search"
+          variant="outlined"
+          ref={(input) => (this.search = input)}
+          onChange={this.handleInputChange}
+        />
+        <p>{this.state.query}</p>
+      </form>
+    );
+  }
 }
